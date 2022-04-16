25c44691402940903ce9088a4e41799f462ebe83
MrToirol <84342903+MrToirol@users.noreply.github.com>
2022-04-16 16:43:28 +0200

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-const interval = setInterval(() => {
-	if (document.getElementsByClassName('playerNameSm playerNameNoHealth').length > 0) {
-		document.getElementsByClassName('compass compass_S compass_flat')[0].remove();
-		document.getElementsByClassName('largeclock timeofday')[0].remove();
-		document.getElementsByClassName('leaflet-bottom leaflet-right')[0].remove();
-		clearInterval(interval);
-	}
-}, 1000);
